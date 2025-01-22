8b5e8e7ebec7ef302d6bbf9fce48ee2876cb489d
dand2008 <dvir.magshimim@gmail.com>
2025-01-22 19:55:22 +0200

    V1

---
+++ function reveal.js
@@ -0,0 +1,27 @@
+// Debounce function to limit the rate of scroll event firing
+function debounce(func, delay) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, delay);
+  };
+}
+
+function reveal() {
+  var reveals = document.querySelectorAll(".main_header");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+// Add a debounced scroll event listener
+window.addEventListener("scroll", debounce(reveal, 50)); // Adjust the delay as needed
